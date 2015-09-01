db4e6e06b54cd217cf5691bc9269d197ae9100cd
Alessandro <alessandromarin80@gmail.com>
2015-09-01 16:24:20 -0400

    date

--- simpleGraph.js
+++ simpleGraph.js
@@ -25,7 +25,7 @@ git push
 //branches
 git status
 git branch zoom
-git remote add master https://github.com/aless80/iGrow
+git remote add zoom https://github.com/aless80/iGrow
 git push origin --delete <branchToDelete>
 git checkout zoom
 
